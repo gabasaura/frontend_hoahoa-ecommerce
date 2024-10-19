c04f4b18820ba126efd1789a62e530681f7d5ec2
gabasaura <gabriela.garin@gmail.com>
2024-10-19 01:21:53 -0300

    homepage checkpoint

--- shop/src/App.tsx
+++ shop/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
@@ -20,7 +21,6 @@ const App: React.FC = () => {
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
       <Footer />
-      <Preloader />
     </Router>
   );
 };
