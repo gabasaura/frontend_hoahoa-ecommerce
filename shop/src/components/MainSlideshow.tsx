099086aec731a35aa8c96ad1ac4beef18bd3b571
gabasaura <gabriela.garin@gmail.com>
2024-10-19 23:40:11 -0300

    swiper slider

--- shop/src/components/MainSlideshow.tsx
+++ shop/src/components/MainSlideshow.tsx
@@ -1,14 +1,42 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 const MainSlideshow: React.FC = () => {
+  const slides = slideshowData.length;
+  
   return (
     <section>
       <div className="container-fluid">
-        <div className="slideshow slide-in arrow-absolute text-white" style={{ height: '80vh' }}>
-          <div className="swiper-wrapper">
-            {slideshowData.map((slide, index) => (
-              <div className="swiper-slide jarallax" key={index}>
-                <img src={slide.image} className="jarallax-img" alt="slideshow" />
+        <Swiper
+          spaceBetween={80}
+          slidesPerView={Math.min(slides, 3)} // Dynamically adjust slidesPerView based on the number of slides
+          loop={slides > 3} // Enable loop only if there are more than 3 slides
+          navigation={{
+            nextEl: '.icon-arrow-right',
+            prevEl: '.icon-arrow-left',
+          }}
+          breakpoints={{
+            300: {
+              slidesPerView: Math.min(slides, 1),
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: Math.min(slides, 2),
+              spaceBetween: 20,
+            },
+            1200: {
+              slidesPerView: Math.min(slides, 3),
+              spaceBetween: 80,
+            },
+          }}
+        >
+          {slideshowData.map((slide, index) => (
+            <SwiperSlide key={index}>
+              <div className="swiper-slide ">
+                <img src={slide.image} alt="slideshow" />
                 <div className="banner-content w-100">
                   <div className="container">
                     <div className="row">
@@ -23,16 +51,16 @@ const MainSlideshow: React.FC = () => {
                   </div>
                 </div>
               </div>
-            ))}
-          </div>
-          <div className="icon-arrow icon-arrow-left text-white">
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        {/* Navigation Arrows */}
+        <div className="icon-arrow icon-arrow-left text-white">
           <FaAngleLeft />
-
-          </div>
-          <div className="icon-arrow icon-arrow-right text-white">
+        </div>
+        <div className="icon-arrow icon-arrow-right text-white">
           <FaAngleRight />
-
-          </div>
         </div>
       </div>
     </section>
@@ -54,10 +82,17 @@ const slideshowData = [
   },
   {
     image: 'images/banner-large-3.jpg',
-    title: 'Sturdy And Multifunction New Gadgets',
+    title: 'Buy Electronic Gadgets at Affordable Prices',
     caption: 'Tech gadgets are all about making your life easier',
     buttonText: 'Shop Now',
   },
+  {
+    image: 'images/banner-large-2.jpg',
+    title: 'Cool Tech Gadgets Collection 2023',
+    caption: 'Tech gadgets are all about making your life easier',
+    buttonText: 'Shop Now',
+  },
+  // Add more slides here to prevent the warning
 ];
 
 export default MainSlideshow;
